6116f21c21d4de3827404b47facec3637bd0de2f
zharinovkv <zharinovkv@gmail.com>
2015-11-22 17:43:45 +0500

    Update politrange_createdb.sql

--- politrange_createdb.sql
+++ politrange_createdb.sql
@@ -3,69 +3,63 @@ create database politrange;
 use politrange;                                              
 
 create table Persons (
-  ID 			int 		    not null 	auto_increment, 
-  Name     		varchar(2048) 	not null	,
-  
+  ID 			  int     		    not null 	auto_increment, 
+  Name      varchar(2048) 	not null	,
+
   primary key   (ID)
 ) character set utf8;
 
-
 create table Keywords (
-  ID       		int 			not null 	auto_increment,
-  Name     		varchar(2048)	not null,
-  PersonID   	int 			not null,
+  ID       	int     			  not null 	auto_increment,
+  Name     	varchar(2048)	  not null,
+  PersonID  int 		    	  not null,
   
   primary key   (ID),
   foreign key   (PersonID) references Persons(ID) 
 ) character set utf8;
 
--- 3. таблица PPR
---   извлечь подстроки с ключевыми словами
---   сохранить в таблицу PPR
---   ID
-
 create table Sites (
-  ID    		int 			not null 	auto_increment,
-  Name    		varchar(2048) 	not null,
+  ID    		int 			      not null 	auto_increment,
+  Name    	varchar(2048) 	not null,
   
   primary key   (ID)
 ) character set utf8;
 
 create table Pages (
-  ID     		int 			not null 	auto_increment,
-  URL    		nvarchar(2048) 	not null,
-  SiteID 		int  			not null,
-  FoundDateTime datetime    	not null,
-  LastScanDate 	datetime      	not null,
+  ID     		    int 			      not null 	auto_increment,
+  URL    		    nvarchar(2048) 	not null,
+  SiteID 		    int  			      not null,
+  FoundDateTime datetime        not null,
+  LastScanDate 	datetime        not null,
   
   primary key   (ID),
   foreign key   (SiteID) references Sites(ID)  
 ) character set utf8;
 
 create table PersonPageRank (
-  PersonID 		int 			not null,
-  PageID      	int 			not null,
-  Rank   		int 			not null,
+  PersonID 	int 			      not null,
+  PageID    int 			      not null,
+  Rank   		int   			    not null,
   
   primary key   (PersonID),
   foreign key   (PersonID) 	references Persons(ID),
-  foreign key   (PageID) 	references Pages(ID) 
+  foreign key   (PageID) 	  references Pages(ID) 
 ) character set utf8;
 
 -- создание остальных таблиц ...
 -- пользователи...
 
 create table Roles (
-  ID     		int 			not null 	auto_increment,
-  Role    		varchar(2048) 	not null,
+  ID     		int 			      not null 	auto_increment,
+  Role    	varchar(2048) 	not null,
   
   primary key   (Id)
 ) character set utf8;
 
 create table Users (
-  ID  			int 			not null 	auto_increment,
-  Name    		varchar(2048) 	not null,
-  RoleId   		int			 	not null,	
+  ID  			int        			not null 	auto_increment,
+  Name    	varchar(2048) 	not null,
+  RoleId   	int			 	      not null,	
   
   primary key   (ID),
   foreign key   (RoleId) references Roles(ID)
