076f0399f176a92b1f8472c27c2bc7da3d936e2f
iGilga <pyua87@gmail.com>
2015-11-24 19:18:36 +0300

    Добавлены модели сущностей и их связи по-умолчанию.

---
+++ src/SuperBug.Politrange.Models/PersonPageRank.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperBug.Politrange.Models
+{
+	public class PersonPageRank
+	{
+		public Person Person { get; set; }
+		public Page Page { get; set; }
+		public int Rank { get; set; }
+	}
+}
