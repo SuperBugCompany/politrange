7008143e9b70a41d5579caa61f3a990551e4b048
iGilga <pyua87@gmail.com>
2015-11-24 19:20:19 +0300

    Добавлен контекст по-умолчанию.

---
+++ src/SuperBug.Politrange.Data/Contexts/PolitRangeContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using SuperBug.Politrange.Models;
+
+namespace SuperBug.Politrange.Data.Contexts
+{
+    public class PolitRangeContext: DbContext
+    {
+        public DbSet<Keyword> Keywords { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<PersonPageRank> PersonPageRanks { get; set; }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Site> Sites { get; set; }
+    }
+}
