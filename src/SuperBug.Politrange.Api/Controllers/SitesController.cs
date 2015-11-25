a116ebba3dbb608155f44a98159686a96d8874a9
iGilga <pyua87@gmail.com>
2015-11-25 14:45:05 +0300

    Добавлен контроллер сайт

---
+++ src/SuperBug.Politrange.Api/Controllers/SitesController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Http;
+using SuperBug.Politrange.Services.Sites;
+
+namespace SuperBug.Politrange.Api.Controllers
+{
+    public class SiteController: ApiController
+    {
+        private readonly ISiteService siteService;
+
+        public SiteController(ISiteService siteService)
+        {
+            this.siteService = siteService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            var sites = siteService.GetAll();
+
+            return Ok(sites);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var site = siteService.GetSitebyId(id);
+
+            return Ok(site);
+        }
+    }
+}
