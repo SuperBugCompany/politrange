628073fc45a941a7b10ea1b5ee8e0367996b4443
iGilga <pyua87@gmail.com>
2015-11-25 14:40:55 +0300

    Добавлен репозиторий сайта.

---
+++ src/SuperBug.Politrange.Data/Repositories/SiteRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SuperBug.Politrange.Data.Contexts;
+using SuperBug.Politrange.Models;
+
+namespace SuperBug.Politrange.Data.Repositories
+{
+    public class SiteRepository: ISiteRepository
+    {
+        private readonly IPolitrangeContext politrangeContext;
+
+        public SiteRepository(IPolitrangeContext politrangeContext)
+        {
+            this.politrangeContext = politrangeContext;
+        }
+
+        public IEnumerable<Site> GetAllSite()
+        {
+            return politrangeContext.Sites.ToList();
+        }
+
+        public Site GetSiteById(int id)
+        {
+            return politrangeContext.Sites.FirstOrDefault(x => x.SiteId == id);
+        }
+    }
+}
