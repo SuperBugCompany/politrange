2088ae874323579b43b297a35a0c05d73d23b89d
iGilga <pyua87@gmail.com>
2015-11-25 14:37:51 +0300

    Добавлен первичный ключ

--- src/SuperBug.Politrange.Models/PersonPageRank.cs
+++ src/SuperBug.Politrange.Models/PersonPageRank.cs
@@ -1,7 +1,10 @@
-﻿namespace SuperBug.Politrange.Models
+﻿using System.Security.AccessControl;
+
+namespace SuperBug.Politrange.Models
 {
 	public class PersonPageRank
 	{
+	    public int PersonPageRankId { get; set; }
 		public Person Person { get; set; }
 		public Page Page { get; set; }
 		public int Rank { get; set; }
