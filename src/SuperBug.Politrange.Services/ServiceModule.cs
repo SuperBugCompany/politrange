a18b83ca2eb0c5ab83ff7a2dfed8f254a5bc5dfa
iGilga <pyua87@gmail.com>
2015-11-25 14:43:51 +0300

    Регистрация сервиса.

--- src/SuperBug.Politrange.Services/ServiceModule.cs
+++ src/SuperBug.Politrange.Services/ServiceModule.cs
@@ -1,12 +1,14 @@
 ﻿using Autofac;
+using SuperBug.Politrange.Services.Sites;
+using SuperBug.Politrange.Services.States;
 
 namespace SuperBug.Politrange.Services
 {
-	public class ServiceModule:Module
-	{
-		protected override void Load(ContainerBuilder builder)
-		{
-			base.Load(builder);
-		}
-	}
+    public class ServiceModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<SiteService>().As<ISiteService>();
+        }
+    }
 }
