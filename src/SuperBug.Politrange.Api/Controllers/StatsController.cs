a8afb4ed02bd94d4cc2cc488a079fe4e2557cebb
iGilga <pyua87@gmail.com>
2015-11-26 18:12:32 +0300

    Добавлен контроллер статистики. Реализована общая статистика.

---
+++ src/SuperBug.Politrange.Api/Controllers/StatsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Http;
+using SuperBug.Politrange.Services.States;
+
+namespace SuperBug.Politrange.Api.Controllers
+{
+    public class StatController: ApiController
+    {
+        private readonly IStatService statService;
+
+        public StatController(IStatService statService)
+        {
+            this.statService = statService;
+        }
+
+        public IHttpActionResult GetStatBySiteId(int id)
+        {
+            var ratings = statService.GetPageRanksBySite(id);
+
+            return Ok(ratings);
+        }
+    }
+
+    
+}
