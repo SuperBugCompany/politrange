1099d25740da67ce50e30bfd88c4eda3f671b234
zharinovkv <zharinovkv@gmail.com>
2015-11-28 13:46:48 +0500

    Потеряны урлы, Текущая задача: передавать урлы в Лист параграфов.

---
+++ politrange/ListUrlsJneGenerate/Element.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListUrlsJneGenerate
+{
+    public class Element
+    {
+        public string Link { get; set; }
+        public string Name { get; set; }
+        public int Frequenty { get; set; }
+
+        public Element(string link, string Name, int frequenty)
+        {
+            this.Link = link;
+            this.Name = Name;
+            this.Frequenty = frequenty;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[{0}, {1}, {2}]", Link, Name, Frequenty);
+        }
+
+    }
+}
