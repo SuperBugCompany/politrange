1188d0808be3cb29bd4de0396f073d64bc0024a2
zharinovkv <zharinovkv@gmail.com>
2015-12-03 16:37:52 +0500

    опыт.

---
+++ RssClass/RssClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace rssсclass
+{ 
+    [Serializable]
+    [XmlRoot(ElementName = "rss")]
+    public class RssClass
+    {
+        [XmlElement(ElementName = "channel")]
+        public ChannelClass Channel { get; set; }
+    }
+
+    public class ChannelClass
+    {
+        [XmlElement(ElementName = "item")]
+        //public List<ItemClass> Items { get; set; }
+        public List<LinkClass> Links { get; set; }
+    }
+
+    public class ItemClass
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+    }
+
+    public class LinkClass
+    {
+        [XmlElement(ElementName = "link")]
+        public string Link { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format(Link);
+        }
+    }
+}
