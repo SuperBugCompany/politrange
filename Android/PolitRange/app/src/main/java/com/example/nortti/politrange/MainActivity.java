b32e0e7741fe6251097f8b959772b4f4137d00eb
Nortti <a.khorevich@gmail.com>
2015-12-12 13:33:46 +0300

    menu fix

--- Android/PolitRange/app/src/main/java/com/example/nortti/politrange/MainActivity.java
+++ Android/PolitRange/app/src/main/java/com/example/nortti/politrange/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.nortti.politrange;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TabHost;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
@@ -39,19 +37,23 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu,menu);
-        return super.onCreateOptionsMenu(menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.info:
-            Toast.makeText(this,"Info about team",Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.info) {
+           Toast.makeText(this,"SuperBug Company Info",Toast.LENGTH_SHORT).show();
         }
+
+        return super.onOptionsItemSelected(item);
     }
 }
