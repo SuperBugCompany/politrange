15ccfb67b1418340eaaa100ab655087664e19f8d
Nortti <a.khorevich@gmail.com>
2015-12-20 23:59:14 +0300

    clear

---
+++ Android/PolitRange/app/src/main/java/com/example/nortti/politrange/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.nortti.politrange;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost tabs = (TabHost) findViewById(R.id.tabHost);
+
+        tabs.setup();
+
+        TabHost.TabSpec spec = tabs.newTabSpec("tag1");
+
+        spec.setContent(R.id.tab1);
+        spec.setIndicator(getResources().getString(R.string.general));
+        tabs.addTab(spec);
+
+        spec = tabs.newTabSpec("tag2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator(getResources().getString(R.string.daily));
+        tabs.addTab(spec);
+
+        tabs.setCurrentTab(0);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.info) {
+          InfoDialog();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void InfoDialog(){
+        View dial = getLayoutInflater().inflate(R.layout.dialog,null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.CustomDialog);
+        builder.setView(dial).setCancelable(true).show();
+        TextView link = (TextView) dial.findViewById(R.id.link);
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri adress = Uri.parse("https://superbugcompany.github.io/politrange/website/");
+                Intent openLink = new Intent(Intent.ACTION_VIEW,adress);
+                startActivity(openLink);
+            }
+        });
+
+    }
+
+}
---
+++ Android/PolitRange/app/src/main/java/com/example/nortti/politrange/intefaces/impls/SitesCatalog.java
@@ -0,0 +1,22 @@
+package com.example.nortti.politrange.intefaces.impls;
+
+
+import com.example.nortti.politrange.intefaces.ICatalog;
+import com.example.nortti.politrange.R;
+import com.example.nortti.politrange.objects.Site;
+
+import java.util.ArrayList;
+
+public class SitesCatalog implements ICatalog{
+
+  private ArrayList<Site> catalogList = new ArrayList<Site>();
+
+    public ArrayList<Site> getCatalogList() {
+        return catalogList;
+    }
+    public void populateData(){
+        catalogList.add(new Site(1,"Lenta.ru",R.drawable.ic_lenta));
+        catalogList.add(new Site(2,"Rbc.ru",R.drawable.ic_rbc));
+        catalogList.add(new Site(3,"Tass.ru",R.drawable.ic_tass));
+    }
+}
